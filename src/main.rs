23c52c3d29c8324ba2ed75f638a35e1382ab0208
garasubo <garasubo@gmail.com>
2020-07-05 10:45:17 +0900

    remove old help message

--- src/main.rs
+++ src/main.rs
@@ -308,34 +308,6 @@ impl Drop for PipeIntercepter {
 }
 
 lazy_static! {
-    static ref USAGE: String = format!(
-        "
-Allow the command handle selected parts of the standard input, and bypass other parts.
-
-Usage:
-  {cmd} -g <pattern> [-oGsvz] [--] [<command>...]
-  {cmd} -f <list> [-d <delimiter> | -D <pattern>] [-svz] [--] [<command>...]
-  {cmd} -c <list> [-svz] [--] [<command>...]
-  {cmd} -l <list> [-svz] [--] [<command>...]
-  {cmd} --help | --version
-
-Options:
-  --help          Display this help and exit
-  --version       Show version and exit
-  -g <pattern>    Select lines that match the regular expression <pattern>
-  -o              -g selects only matched parts
-  -G              -g adopts Oniguruma regular expressions
-  -f <list>       Select only these white-space separated fields
-  -d <delimiter>  Use <delimiter> for field delimiter of -f
-  -D <pattern>    Use regular expression <pattern> for field delimiter of -f
-  -c <list>       Select only these characters
-  -l <list>       Select only these lines
-  -s              Execute command for each selected part
-  -v              Invert the sense of selecting
-  -z              Line delimiter is NUL instead of newline
-",
-        cmd = CMD
-    );
     static ref REGEX_WS: Regex = Regex::new("\\s+").unwrap();
     static ref DEFAULT_HIGHLIGHT: String = match env::var("TEIP_HIGHLIGHT") {
         Ok(v) => v,
