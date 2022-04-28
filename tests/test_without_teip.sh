97ac88888744be9dfff7c3e8ec8fdea525d55269
Yasuhiro Yamada <yamada@gr3.ie>
2022-04-28 18:06:32 +0000

    update test_without_teip.sh

--- tests/test_without_teip.sh
+++ tests/test_without_teip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -u
-# grep -n -C 3 admin < test_secure | awk -F '[-:]' '{print $1}' | awk NF | awk '{ do{ for(s=e=$1; (r=getline)>0 && $1<=e+1; e=$1); print s==e ? s : s","e }while(r>0) }'| awk '{print NR"s/./@/g"}' | sed -f- test_secure > tmp
-grep -n -C 3 admin < test_secure | awk -F '[-:]' '{print $1}' | awk NF | awk '{print NR"s/./@/g"}' | sed -f- test_secure > tmp
+grep -n -C 3 admin < test_secure | awk -F '[-:]' '{print $1}' | awk NF | awk '{ do{ for(s=e=$1; (r=getline)>0 && $1<=e+1; e=$1); print s==e ? s : s","e }while(r>0) }'| awk '{print $0"s/./@/g"}' | sed -f- test_secure > tmp
+# grep -n -C 3 admin < test_secure | awk -F '[-:]' '{print $1}' | awk NF | awk '{print $0"s/./@/g"}' | sed -f- test_secure > tmp
 echo "sha1sum = $(sha1sum tmp)"
