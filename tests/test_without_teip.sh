651f26b935d5870e42f8fb307017131997788763
Yasuhiro Yamada <yamada@gr3.ie>
2022-04-28 14:06:58 +0000

    [bench] update test without teip

--- tests/test_without_teip.sh
+++ tests/test_without_teip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -u
 readonly THIS_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-grep -n -C 3 admin < "$THIS_DIR"/test_secure | awk -F '[-:]' '{print $1}' | awk 'NF{print NR"s/./@/g"}' | sed -f- test_secure > /dev/null
+grep -n -C 3 admin < "$THIS_DIR"/test_secure | awk -F '[-:]' '{print $1}' | awk 'NF{print NR"s/./@/g"}' | sed -f- test_secure > tmp
+echo "sha1sum = $(sha1sum tmp)"
