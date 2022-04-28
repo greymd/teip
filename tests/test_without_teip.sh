859abff330f7fff37deafa19804f8f6dacbabc1e
Yasuhiro Yamada <yamada@gr3.ie>
2022-04-28 14:01:43 +0000

    [bench] Add tests without teip

---
+++ tests/test_without_teip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -u
+readonly THIS_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+grep -n -C 3 admin < "$THIS_DIR"/test_secure | awk -F '[-:]' '{print $1}' | awk 'NF{print NR"s/./@/g"}' | sed -f- test_secure > /dev/null
