1a0a89fdb997526ded70fc19ea5d78dee90a0d61
Yasuhiro Yamada <yamada@gr3.ie>
2023-02-25 16:57:00 +0000

    CSV tests for newline/comma/quotations

--- tests/lib.rs
+++ tests/lib.rs
@@ -589,6 +589,42 @@ mod cmdtest {
     }
 
     #[test]
+    fn test_csv() {
+        let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.args(&["--csv", "-f", "2", "--", SED_CMD, "s/./@/g"])
+            .write_stdin("名前,作者,ノート\n１レコード目,\"あいう\nえお\",かきく\n２レコード目,\"さしす\nせそ\",\"たちつ\nてと\"\n")
+            .assert()
+            .stdout("名前,@@,ノート\n１レコード目,@@@@\n@@@,かきく\n２レコード目,@@@@\n@@@,\"たちつ\nてと\"\n");
+    }
+
+    #[test]
+    fn test_csv_end_nolf() {
+        let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.args(&["--csv", "-f", "3", "--", SED_CMD, "s/./@/g"])
+            .write_stdin("名前,作者,ノート\n１レコード目,\"あいう\nえお\",かきく\n２レコード目,\"さしす\nせそ\",\"たちつ\nてと\"")
+            .assert()
+            .stdout("名前,作者,@@@\n１レコード目,\"あいう\nえお\",@@@\n２レコード目,\"さしす\nせそ\",@@@@\n@@@");
+    }
+
+    #[test]
+    fn test_csv_non_delim_comma() {
+        let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.args(&["--csv", "-f", "1", "--", SED_CMD, "s/./@/g"])
+            .write_stdin("\"名,前\",作者,ノート\n１レコード目,\"あいう\nえお\",かきく\n\"２レコード,目\",\"さしす\nせそ\",\"たちつ\nてと\"")
+            .assert()
+            .stdout("@@@@@,作者,ノート\n@@@@@@,\"あいう\nえお\",かきく\n@@@@@@@@@,\"さしす\nせそ\",\"たちつ\nてと\"");
+    }
+
+    #[test]
+    fn test_csv_double_quotation() {
+        let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.args(&["--csv", "-f", "2", "--", SED_CMD, "s/./@/g"])
+            .write_stdin("名前,\"作\"\"者\",ノート\n１レコード目,\"あ\"\"いう\"\"\nえお\",かきく\n２レコード目,\"\"\"さしす\n\"\"せそ\",\"たちつ\nてと\"\n")
+            .assert()
+            .stdout("名前,@@@@@@,ノート\n１レコード目,@@@@@@@@\n@@@,かきく\n２レコード目,@@@@@@\n@@@@@,\"たちつ\nてと\"\n");
+    }
+
+    #[test]
     fn test_solid_field() {
         let mut cmd = assert_cmd::Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
         cmd.args(&["-s", "-d", ",", "-f", "1,2,4", SED_CMD, "s/./_/"])
